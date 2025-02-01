4ed4523f19e3a0b3f94ab30fe5d87f9fb0fa86c6
Jim Brighter <jim-brighter@users.noreply.github.com>
2025-02-01 16:38:03 -0500

    migrate to cloudfront distribution (#275)
    * migrate to distribution
    
    * remove semicolons

--- cdk/lib/verytinystack.ts
+++ cdk/lib/verytinystack.ts
@@ -1,35 +1,36 @@
-import { Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Table, BillingMode, TableEncryption, AttributeType, ProjectionType } from 'aws-cdk-lib/aws-dynamodb';
-import { BackupPlan, BackupResource } from 'aws-cdk-lib/aws-backup';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
-import { LambdaIntegration, LambdaRestApi, SecurityPolicy } from 'aws-cdk-lib/aws-apigateway';
-import { ApiGateway, CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
-import { BlockPublicAccess, Bucket, BucketAccessControl, BucketEncryption } from 'aws-cdk-lib/aws-s3';
-import { CloudFrontAllowedCachedMethods, CloudFrontAllowedMethods, CloudFrontWebDistribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { Table, BillingMode, TableEncryption, AttributeType, ProjectionType } from 'aws-cdk-lib/aws-dynamodb'
+import { BackupPlan, BackupResource } from 'aws-cdk-lib/aws-backup'
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53'
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager'
+import { LambdaIntegration, LambdaRestApi, SecurityPolicy } from 'aws-cdk-lib/aws-apigateway'
+import { ApiGateway, CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets'
+import { BlockPublicAccess, Bucket, BucketAccessControl, BucketEncryption } from 'aws-cdk-lib/aws-s3'
+import { AllowedMethods, CachedMethods, CachePolicy, Distribution, SecurityPolicyProtocol, SSLMethod, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront'
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment'
+import { S3StaticWebsiteOrigin } from 'aws-cdk-lib/aws-cloudfront-origins'
 
 const SUBMITTER_INDEX = 'SubmitterIndex'
 
 export class VeryTinyStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Hosted Zone
     const hostedZone = HostedZone.fromLookup(this, 'HostedZone', {
       domainName: 'verytiny.link'
-    });
+    })
 
     // SSL Certificate
     const cert = new Certificate(this, 'VeryTinyCertificate', {
       domainName: 'verytiny.link',
       subjectAlternativeNames: ['*.verytiny.link'],
       validation: CertificateValidation.fromDns(hostedZone)
-    });
+    })
 
     // Dynamo
     const records = new Table(this, 'VeryTinyUrls', {
@@ -48,7 +49,7 @@ export class VeryTinyStack extends Stack {
       deletionProtection: true,
       pointInTimeRecovery: true,
       timeToLiveAttribute: 'ttl'
-    });
+    })
 
     records.addGlobalSecondaryIndex({
       indexName: SUBMITTER_INDEX,
@@ -57,14 +58,14 @@ export class VeryTinyStack extends Stack {
         type: AttributeType.STRING
       },
       projectionType: ProjectionType.ALL
-    });
+    })
 
-    const plan = BackupPlan.daily35DayRetention(this, 'BackupPlan');
+    const plan = BackupPlan.daily35DayRetention(this, 'BackupPlan')
     plan.addSelection('BackupPlanSelection', {
       resources: [
         BackupResource.fromDynamoDbTable(records)
       ]
-    });
+    })
 
     // Lambda
     const defaultErrorLambda = new Function(this, 'DefaultErrorHandler', {
@@ -80,7 +81,7 @@ export class VeryTinyStack extends Stack {
         }
       }
       `)
-    });
+    })
 
     const tinyLinkLambda = new NodejsFunction(this, 'VeryTinyLambda', {
       runtime: Runtime.NODEJS_22_X,
@@ -95,9 +96,9 @@ export class VeryTinyStack extends Stack {
         SUBMITTER_INDEX
       },
       logRetention: RetentionDays.THREE_DAYS
-    });
+    })
 
-    records.grantReadWriteData(tinyLinkLambda);
+    records.grantReadWriteData(tinyLinkLambda)
 
     // API
     const restApi = new LambdaRestApi(this, 'VeryTinyApi', {
@@ -108,7 +109,7 @@ export class VeryTinyStack extends Stack {
         domainName: 'verytiny.link',
         securityPolicy: SecurityPolicy.TLS_1_2
       }
-    });
+    })
 
     const tinyLambdaIntegration = new LambdaIntegration(tinyLinkLambda)
 
@@ -116,23 +117,23 @@ export class VeryTinyStack extends Stack {
       allowOrigins: ['*'],
       allowHeaders: ['*'],
       allowCredentials: true
-    });
+    })
 
-    restApi.root.addMethod('GET', tinyLambdaIntegration);
-    restApi.root.addMethod('POST', tinyLambdaIntegration);
+    restApi.root.addMethod('GET', tinyLambdaIntegration)
+    restApi.root.addMethod('POST', tinyLambdaIntegration)
 
-    const urls = restApi.root.addResource('{key}');
+    const urls = restApi.root.addResource('{key}')
     urls.addCorsPreflight({
       allowOrigins: ['*'],
       allowHeaders: ['*'],
       allowCredentials: true
-    });
-    urls.addMethod('GET', tinyLambdaIntegration);
+    })
+    urls.addMethod('GET', tinyLambdaIntegration)
 
     new ARecord(this, 'VeryTinyRootRecord', {
       zone: hostedZone,
       target: RecordTarget.fromAlias(new ApiGateway(restApi))
-    });
+    })
 
     // UI Bucket
     const uiBucket = new Bucket(this, 'VeryTinyBucket', {
@@ -150,54 +151,42 @@ export class VeryTinyStack extends Stack {
         expiredObjectDeleteMarker: true,
         noncurrentVersionExpiration: Duration.days(3)
       }]
-    });
+    })
 
     // CloudFront
-    const distribution = new CloudFrontWebDistribution(this, 'VeryTinyCloudfront', {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: uiBucket
-          },
-          behaviors: [
-            {
-              isDefaultBehavior: true,
-              pathPattern: '*',
-              compress: true,
-              allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
-              cachedMethods: CloudFrontAllowedCachedMethods.GET_HEAD_OPTIONS,
-              defaultTtl: Duration.days(90),
-              minTtl: Duration.days(30),
-              maxTtl: Duration.days(365)
-            }
-          ]
-        }
-      ],
-      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    const distribution = new Distribution(this, 'VeryTinyCloudfront', {
+      defaultBehavior: {
+        compress: true,
+        origin: new S3StaticWebsiteOrigin(uiBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachedMethods: CachedMethods.CACHE_GET_HEAD_OPTIONS,
+        cachePolicy: new CachePolicy(this, 'CachePolicy', {
+          defaultTtl: Duration.days(90),
+          minTtl: Duration.days(30),
+          maxTtl: Duration.days(365)
+        })
+      },
+      certificate: cert,
+      sslSupportMethod: SSLMethod.SNI,
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      domainNames: ['home.verytiny.link'],
       defaultRootObject: 'index.html',
-      errorConfigurations: [
+      errorResponses: [
         {
-          errorCode: 404,
+          httpStatus: 404,
           responsePagePath: '/',
-          responseCode: 200,
-          errorCachingMinTtl: Duration.days(30).toSeconds()
+          responseHttpStatus: 200,
+          ttl: Duration.days(30)
         },
         {
-          errorCode: 403,
+          httpStatus: 403,
           responsePagePath: '/',
-          responseCode: 200,
-          errorCachingMinTtl: Duration.days(30).toSeconds()
+          responseHttpStatus: 200,
+          ttl: Duration.days(30)
         }
-      ],
-      viewerCertificate: {
-        aliases: ['home.verytiny.link'],
-        props: {
-          minimumProtocolVersion: 'TLSv1.2_2021',
-          sslSupportMethod: 'sni-only',
-          acmCertificateArn: cert.certificateArn
-        }
-      }
-    });
+      ]
+    })
 
     // UI Deployment
     new BucketDeployment(this, 'VeryTinyBucketDeployment', {
@@ -205,7 +194,7 @@ export class VeryTinyStack extends Stack {
       exclude: ['build.js'],
       destinationBucket: uiBucket,
       distribution: distribution
-    });
+    })
 
     new ARecord(this, 'UIRecord', {
       zone: hostedZone,
@@ -213,6 +202,6 @@ export class VeryTinyStack extends Stack {
       target: {
         aliasTarget: new CloudFrontTarget(distribution)
       }
-    });
+    })
   }
 }
--- ui/js/script.js
+++ ui/js/script.js
@@ -1,18 +1,18 @@
 const validateUrl = (url) => {
-    document.getElementById('url').style.removeProperty('border');
+    document.getElementById('url').style.removeProperty('border')
 
     if (url == null || url.length === 0) {
-        document.getElementById('url').style.border = '2px solid red';
-        throw 'Invalid URL';
+        document.getElementById('url').style.border = '2px solid red'
+        throw 'Invalid URL'
     }
 }
 
 const addSpinner = () => {
-    const spinner = document.createElement('div');
-    spinner.classList.add('spinner');
-    spinner.id = 'loading-spinner';
+    const spinner = document.createElement('div')
+    spinner.classList.add('spinner')
+    spinner.id = 'loading-spinner'
 
-    document.getElementById('response-container').appendChild(spinner);
+    document.getElementById('response-container').appendChild(spinner)
 }
 
 const fetchApi = async (url) => {
@@ -24,42 +24,42 @@ const fetchApi = async (url) => {
         body: JSON.stringify({
             url
         })
-    });
+    })
 
-    const data = await response.json();
+    const data = await response.json()
 
     if (!response.ok) {
-        alert(data.errorMessage);
-        document.getElementById('loading-spinner').remove();
-        throw 'Error from API';
+        alert(data.errorMessage)
+        document.getElementById('loading-spinner').remove()
+        throw 'Error from API'
     }
 
-    return data;
+    return data
 }
 
 const createAndAppendLink = (shortId) => {
-    const veryTinyUrl = `https://verytiny.link/${shortId}`;
+    const veryTinyUrl = `https://verytiny.link/${shortId}`
 
-    const link = document.createElement('a');
-    link.href = veryTinyUrl;
-    link.textContent = veryTinyUrl;
-    link.title = veryTinyUrl;
-    link.target = '_blank';
-    link.rel = 'noopener noreferrer';
-    link.style.display = 'block';
+    const link = document.createElement('a')
+    link.href = veryTinyUrl
+    link.textContent = veryTinyUrl
+    link.title = veryTinyUrl
+    link.target = '_blank'
+    link.rel = 'noopener noreferrer'
+    link.style.display = 'block'
 
-    document.getElementById('loading-spinner').remove();
-    document.getElementById('response-container').appendChild(link);
+    document.getElementById('loading-spinner').remove()
+    document.getElementById('response-container').appendChild(link)
 }
 
 const onSubmit = async (e) => {
-    const url = document.getElementById('url').value;
+    const url = document.getElementById('url').value
 
-    validateUrl(url);
+    validateUrl(url)
 
-    addSpinner();
+    addSpinner()
 
-    const data = await fetchApi(url);
+    const data = await fetchApi(url)
 
-    createAndAppendLink(data.shortId);
+    createAndAppendLink(data.shortId)
 }
