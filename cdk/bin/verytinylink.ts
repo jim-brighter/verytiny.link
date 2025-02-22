944822009b9ccbf3eec3676c1897b92e0c2f70a9
Jim Brighter <jim-brighter@users.noreply.github.com>
2025-02-22 14:08:24 -0500

    west deployment (#282)

--- cdk/bin/verytinylink.ts
+++ cdk/bin/verytinylink.ts
@@ -2,12 +2,22 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { VeryTinyStack } from '../lib/verytinystack'
+import { VeryTinyStackWest } from '../lib/verytinystackwest'
 
 const app = new cdk.App()
 
-new VeryTinyStack(app, 'VeryTinyLink', {
+const east = new VeryTinyStack(app, 'VeryTinyLink', {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION
-    }
+        region: 'us-east-1'
+    },
+    crossRegionReferences: true
+})
+
+const west = new VeryTinyStackWest(app, 'VeryTinyLinkWest', east.hostedZone, east.records, {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: 'us-west-2'
+    },
+    crossRegionReferences: true
 })
