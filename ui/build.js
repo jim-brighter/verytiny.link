446f54460f8d663cf5b655c28bc52e144a0ec797
Jim Brighter <jim-brighter@users.noreply.github.com>
2025-03-08 15:14:46 -0500

    update pipeline (#287)

--- ui/build.js
+++ ui/build.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const fileReplacements = []
 const timestamp = Date.now()
@@ -7,41 +8,62 @@ const isValidFile = (filename) => {
   return filename !== 'build.js' && (filename.endsWith('.css') || filename.endsWith('.js') || filename.endsWith('.html'))
 }
 
+const copyFileWithDir = (source, dest) => {
+  const dir = path.dirname(dest)
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
+  }
+
+  fs.copyFileSync(source, dest)
+}
+
+if (fs.existsSync('dist')) {
+  fs.rmSync('dist', {
+    recursive: true
+  })
+}
+
+fs.mkdirSync('dist')
+
 fs.readdirSync('.', {
   recursive: true
 })
-.filter((entry) => isValidFile(entry))
-.forEach((oldFile) => {
-  if (oldFile === 'index.html') {
-    return
-  }
+  .filter((entry) => isValidFile(entry))
+  .forEach((oldFile) => {
+    if (oldFile === 'index.html') {
+      copyFileWithDir(oldFile, `dist/${oldFile}`)
+      return
+    }
 
-  const splitName = oldFile.split('.')
-  const newFile = `${splitName[0]}.${timestamp}.${splitName[1]}`
+    const splitName = oldFile.split('.')
+    const newFile = `${splitName[0]}.${timestamp}.${splitName[1]}`
 
-  fileReplacements.push({
-    oldName: oldFile.split('/').pop(),
-    newName: newFile.split('/').pop()
+    fileReplacements.push({
+      oldName: oldFile.split('/').pop(),
+      newName: newFile.split('/').pop()
+    })
+
+    copyFileWithDir(oldFile, `dist/${newFile}`)
   })
 
-  fs.renameSync(oldFile, newFile)
-})
-
-fs.readdirSync('.', {
+fs.readdirSync('dist', {
   recursive: true
 })
-.filter((entry) => isValidFile(entry))
-.forEach((filename) => {
-  let contents = fs.readFileSync(filename, {
-    encoding: 'utf-8'
+  .filter((entry) => isValidFile(entry))
+  .forEach((filename) => {
+    let contents = fs.readFileSync(`dist/${filename}`, {
+      encoding: 'utf-8'
+    })
+
+    for (let replacement of fileReplacements) {
+      contents = contents.replace(replacement.oldName, replacement.newName)
+    }
+
+    fs.writeFileSync(`dist/${filename}`, contents, {
+      encoding: 'utf-8',
+      flush: true
+    })
   })
 
-  for (let replacement of fileReplacements) {
-    contents = contents.replace(replacement.oldName, replacement.newName)
-  }
-
-  fs.writeFileSync(filename, contents, {
-    encoding: 'utf-8',
-    flush: true
-  })
-})
+console.log('UI Built Successfully')
